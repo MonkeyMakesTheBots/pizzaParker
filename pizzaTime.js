5b6b6a26183dfdd16fbf7eaf22261721436495f0
MonkeyMakesTheBots <78673305+MonkeyMakesTheBots@users.noreply.github.com>
2021-10-13 10:38:43 -0400

    Add files via upload

---
+++ pizzaTime.js
@@ -0,0 +1,8 @@
+// Change background to Color red
+	function setRed() {
+		pageTitle.style.background= "Red";
+}
+// Change background to Color white
+	function setWhite() {
+		pageTitle.style.background= "Linen";
+}
